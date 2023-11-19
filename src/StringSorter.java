e3a1269b93709a0b69159c72e4bfc86e9ea18b70
mrInsaf <mr.insaf@list.ru>
2023-11-19 13:12:14 +0300

    pometil functionalInterface statement lambda replaced with expression lambda

--- src/StringSorter.java
+++ src/StringSorter.java
@@ -1,10 +1,8 @@
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class StringSorter {
     public List<String> sortStrings(List<String> inputStrings, List<String> outputStrings) {
-        Sorting sorter = (String str) -> {return str.contains("a");};
+        Sorting sorter = (String str) -> str.contains("a");
         for (String str: inputStrings) {
             if (sorter.sort(str))
                 outputStrings.add(str);
@@ -14,6 +12,7 @@ public class StringSorter {
     }
 }
 
+@FunctionalInterface
 interface Sorting {
     boolean sort(String string);
 }
